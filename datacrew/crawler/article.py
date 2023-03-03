2cff2d086d1e10ccb8350b7baf646e1b00fe803a
jaewilson07 <jaewilson07@gmail.com>
2023-03-03 14:54:49 +0000

    updated kb

--- datacrew/crawler/article.py
+++ datacrew/crawler/article.py
@@ -9,7 +9,6 @@ import os
 import datetime as dt
 from dataclasses import dataclass, field
 import urllib.parse as url_parse
-import urlparse
 
 from bs4 import BeautifulSoup
 import selenium.webdriver
@@ -141,9 +140,7 @@ class Article_KB(Article):
         
         super().__init__(url = url, base_url=base_url, soup=soup, url_entity_prefix=url_entity_prefix)
 
-        url_path = urlparse.urlparse(url)
-
-        self.url_id = os.path.split(url_path)[1]
+        self.url_id = self.url.split('/')[-1]
 
 
         try:
